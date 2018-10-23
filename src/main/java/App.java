aa4ececfff54368ff2b3676fc48278e6dfc6a9ad
abbiemurugi <abigaelmurugi@gmail.com>
2018-10-23 14:05:05 +0300

    added route for first page

---
+++ src/main/java/App.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import static spark.debug.DebugScreen.enableDebugScreen;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+        enableDebugScreen();
+//HEROKU
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        setPort(port);
+
+    }
+
+    get("/", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("stylists", Stylist.all());
+        model.put("template", "templates/stylists.vtl");
+        return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/Clients", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        String name = request.queryParams("name");
+        Clients newClients = new Clients(name);
+        newClients.save();
+        model.put("template", "templates/category-success.vtl");
+        return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+}
