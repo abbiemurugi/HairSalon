ea703ef17fe31742d8582157d3c9c7e225e4c747
abbiemurugi <abigaelmurugi@gmail.com>
2018-10-23 12:38:35 +0300

    added test for saving clients and ids

--- src/test/java/ClientsTest.java
+++ src/test/java/ClientsTest.java
@@ -18,4 +18,36 @@ public class ClientsTest {
             con.createQuery(deleteClientsQuery).executeUpdate();
         }
     }
+
+    @Test
+    public void equals_returnsTrueIfNamesAretheSame() {
+        Clients firstClients = new Clients("Jane", "Muthoni", "Mwangi", "janemuthoni@gmail.com", "40", "Nairobi");
+        Clients secondClients = new Clients("Jack", "Ochieng", "Otieno", "jackochieng@gmail.com", "36", "Nairobi");
+        assertTrue(firstClients.equals(secondClients));
+    }
+
+    @Test
+    public void save_savesIntoDatabase_true() {
+        Clients myClients = new Clients("Jane", "Muthoni", "Mwangi", "janemuthoni@gmail.com", "40", "Nairobi");
+        myClients.save();
+        assertTrue(Clients.all().get(0).equals(myClients));
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfClients_true() {
+        Clients firstClients = new Clients("Jane", "Muthoni", "Mwangi", "janemuthoni@gmail.com", "40", "Nairobi");
+        firstClients.save();
+        Clients secondClients = new Clients("Jack", "Ochieng", "Otieno", "jackochieng@gmail.com", "36", "Nairobi");
+        secondClients.save();
+        assertEquals(true, Clients.all().get(0).equals(firstClients));
+        assertEquals(true, Clients.all().get(1).equals(secondClients));
+    }
+
+    @Test
+    public void save_assignsIdToObject() {
+        Category myCategory = new Category("Household chores");
+        myCategory.save();
+        Category savedCategory = Category.all().get(0);
+        assertEquals(myCategory.getId(), savedCategory.getId());
+    }
 }
