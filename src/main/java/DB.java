a7c4cc96da5a235ed1ce083f7391f2131a25fa1b
abbiemurugi <abigaelmurugi@gmail.com>
2018-10-24 00:56:45 +0300

    added connection with db

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,30 @@
 import org.sql2o.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "kingcubby", "murugi254" );
+    public static Sql2o sql2o;
+    private static URI dbUri;
+    static Logger logger = LoggerFactory.getLogger(DB.class);
+    static {
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/hair_salon");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "kingcubby" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "murugi254" : dbUri.getUserInfo().split(":")[1];
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) {
+            logger.error("Unable to connect to database.");
+        }
+    }
 }
+//    Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "kingcubby", "murugi254" );
+//}
